37d6e6066099785c995f3a3a2bef795d8dec8875
Marcus <mlim0028@monash.edu.au>
2019-08-25 03:34:35 +1000

    SWIPING IS GOOOOOOOOD
    Sniping Tim

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, View } from 'react-native'
+import { Button, SafeAreaView, StyleSheet, View } from 'react-native'
 import Swiper from 'react-native-deck-swiper'
 import { Card } from '../components/Card'
 import { HomeScreenPics, MyListScreenPics } from '../constants/Pics'
@@ -9,26 +9,43 @@ import MyListScreen from './MyListScreen';
 import { GiftedChat } from 'react-native-gifted-chat';
 
 class HomeScreen extends React.Component {
-
-  state = {
-    MyListScreen: []
+  constructor (props) {
+    super(props)
+    this.state = {
+      cards: HomeScreenPics,
+      swipedAllCards: false,
+      swipeDirection: '',
+      cardIndex: 0,
+      MyListScreen: [],
+      infinite: true,
+      verticalSwipe: false,
+      cardIndex: 0
+    }
   }
-
+  emSwipeLeft() {
+    this.swiper.swipeLeft()
+  };
+  emSwipeRight() {
+    this.swiper.swipeRight()
+  };
+  addToListScreen(number){
+  var to_add = HomeScreenPics[number]
+  MyListScreenPics.push(to_add)
+  }
   render() {
       const yesbuttontext = 'Yes';
       
       const nobuttontext = 'No';
-      
     return (
-     
       <SafeAreaView style={styles.container}>
         <Swiper 
-          onSwipedRight={ () => {addToListScreen(3)} }
-          onSwipedLeft={ () => {increment(3)} }
-          onTapCard = {() => {setID()}} // this isnt working yets
-          cards={HomeScreenPics}
+          ref={(swiper) => this.swiper = swiper}
+          onSwipedRight={(cardIndex) => this.addToListScreen(cardIndex)}
+          onSwiped={(cardIndex) => {console.log(cardIndex)}}
+          // onTapCard = {() => {setID()}} // this isnt working yets
+          cards={this.state.cards}
+          cardIndex={this.state.cardIndex}
           renderCard={Card}
-          infinite
           backgroundColor="#24262A"
           cardHorizontalMargin={0}
           stackSize={2}
@@ -36,10 +53,10 @@ class HomeScreen extends React.Component {
         />
         <View style={styles.buttons}>
           <View>
-            <LikeDislikeButton ButtonText={nobuttontext}></LikeDislikeButton>
+            <Button onPress={() => this.emSwipeLeft()} title={nobuttontext}></Button>
           </View>
           <View>
-            <LikeDislikeButton ButtonText={yesbuttontext}></LikeDislikeButton>
+            <Button onPress={() => this.emSwipeRight()} title={yesbuttontext}></Button>
           </View>
         </View>
       </SafeAreaView>
@@ -47,20 +64,6 @@ class HomeScreen extends React.Component {
   }
 }
 var i = 0
-
-function increment(){
-  i = i + 1
-}
-
-function setID(){
-  console.log("testing")
-}
-
-function addToListScreen(number){
-  var to_add = HomeScreenPics[i]
-  MyListScreenPics.push(to_add)
-  i = i + 1
-}
 
 const styles = StyleSheet.create({
   container: {
