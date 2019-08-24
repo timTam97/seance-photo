2abe596a5cedbc684b5973cd41b5c4be7bf87a25
Marcus <mlim0028@monash.edu.au>
2019-08-25 06:17:33 +1000

    Prep for moonshot

---
+++ constants/Lists.js
@@ -0,0 +1,20 @@
+export var userLists = ({
+  0 : {
+    listCandidates: [],
+  },
+  1 : {
+    listCandidates: [3,2],
+  },
+  2 : {
+    listCandidates: [],
+  },
+  3 : {
+    listCandidates: [],
+  },
+  4 : {
+    listCandidates: [],
+  },
+  5 : {
+    listCandidates: [],
+  },
+})
